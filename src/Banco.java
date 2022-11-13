34ae240a373116c56b48352de6fdcf716252238d
EliasGill <79729891+Elias-Gill@users.noreply.github.com>
2022-11-13 18:06:45 -0300

    carpetas de proyecto (#4)
    * ligerosCambios
    
    * Google Java Format
    
    Co-authored-by: github-actions <>

--- src/Banco.java
+++ src/Banco.java
@@ -4,16 +4,14 @@
  */
 
 /**
- *
  * @author elias
  */
 public class Banco {
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-		// TODO code application logic here
-	}
-
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
 }
