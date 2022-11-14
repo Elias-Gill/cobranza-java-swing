4a2d0039bf0671844fd9637647b7c9ec232ee810
Elias Gill <eliasgill42@gmail.com>
2022-11-14 12:26:44 -0300

    modificando comprobante grafico para su uso con el resto de ventanas

--- src/bankServer/regsYcomprobs/DatosComprobante.java
+++ src/bankServer/regsYcomprobs/DatosComprobante.java
@@ -1,5 +1,14 @@
 package bankServer.regsYcomprobs;
 
-public class DatosComprobante {
+import java.time.*;
 
+import bankServer.data.Cuenta;
+
+public class DatosComprobante {
+   public LocalDate fecha;
+   public Integer id;
+   public Integer monto;
+   public Cuenta cuentaDestino;
+   public String metodo;
+   public String concepto;
 }
--- src/gui/menuPrincipal.java
+++ src/gui/menuPrincipal.java
@@ -1,3 +1,5 @@
 package gui;
 
-public class menuPrincipal {}
+public class menuPrincipal {
+
+}
