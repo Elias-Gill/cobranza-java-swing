ac6bf803147ec002a1a826f6e33b10ce41b88139
androceo123 <105460729+androceo123@users.noreply.github.com>
2022-11-15 19:58:39 -0300

    para rodri
    para rodri

--- src/main/java/src/bankServer/consultasSQL.java
+++ src/main/java/src/bankServer/consultasSQL.java
@@ -1,5 +1,29 @@
 package src.bankServer;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import src.bankServer.data.Cuenta;
+import src.bankServer.data.Tarjeta;
+
 public class consultasSQL {
-    
+
+    public void obtenerCuenta(int nro_cuenta) throws ClassNotFoundException {
+        try {
+            Conexion CC = new Conexion();
+            Connection cn = CC.conectar();
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery("SELCT * FROM Cuenta");
+            while (rs.next()) {
+                if (nro_cuenta == rs.getInt(1)) {
+                    Cuenta retornar = new Cuenta();
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(consultasSQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
