139f466254ea8df65d3769774498f4f31dda3a6c
Elias Gill <eliasgill42@gmail.com>
2022-11-23 23:12:48 -0300

    actualizando esquema y terminado get registros

--- src/main/java/src/bankServer/generadorRegistro.java
+++ src/main/java/src/bankServer/generadorRegistro.java
@@ -1,33 +1,31 @@
 package src.bankServer;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import src.bankServer.regsYcomprobs.DatosComprobante;
 
 public class generadorRegistro {
 
-    private Connection cn;
+	private Connection cn;
 
-    generadorRegistro(Connection c) {
-        cn = c;
-    }
+	generadorRegistro(Connection c) {
+		cn = c;
+	}
 
-    // genera un nuevo registro de alguna operacion dentro de la base de datos
-    public void nuevoRegistro(DatosComprobante datos) throws SQLException {
-        datos.idComprobante = datos.hashCode();
-        PreparedStatement ps = cn.prepareStatement("INSERT INTO registro VALUES(?, ?, ?, ?, ?, ?, ?, ?);");
+	// genera un nuevo registro de alguna operacion dentro de la base de datos
+	public void nuevoRegistro(DatosComprobante datos) throws SQLException {
+		datos.idComprobante = datos.hashCode();
+		PreparedStatement ps = cn.prepareStatement("INSERT INTO registro VALUES(?, ?, ?, ?, ?, ?, ?, ?);");
 
-        ps.setInt(1, datos.idComprobante);
-        ps.setInt(2, datos.cuentaDestino);
-        ps.setInt(3, datos.monto);
-        ps.setDate(4, new Date(datos.fecha.getYear(), datos.fecha.getMonthValue(),
-                datos.fecha.getDayOfMonth()));
-        ps.setString(5, datos.metodo); // forma pago
-        ps.setString(6, datos.concepto);
-        ps.setString(7, datos.idServicio);
-        ps.setInt(8, datos.cuentaOrigen);
-        ps.executeUpdate();
-    }
+		ps.setInt(1, datos.idComprobante);
+		ps.setInt(2, datos.cuentaDestino);
+		ps.setInt(3, datos.monto);
+		ps.setString(4, datos.fecha.toString());
+		ps.setString(5, datos.metodo); // forma pago
+		ps.setString(6, datos.concepto);
+		ps.setString(7, datos.idServicio);
+		ps.setInt(8, datos.cuentaOrigen);
+		ps.executeUpdate();
+	}
 }
--- src/test/java/src/AppTest.java
+++ src/test/java/src/AppTest.java
@@ -2,13 +2,34 @@ package src;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+
+import src.mediador.*;
+import src.bankServer.regsYcomprobs.DatosComprobante;
+
 import org.junit.Test;
 
-/** Unit test for simple App. */
+/**
+ * Unit test for simple App.
+ */
 public class AppTest {
-    /** Rigorous Test :-) */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
+
+	/**
+	 * Rigorous Test :-)
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void shouldAnswerWithTrue() throws Exception {
+		Mediador med = new Mediador();
+		med.IniciarSesion("elias", 5223084);
+		ArrayList<DatosComprobante> res = med.getRegistrosCuenta();
+
+		System.out.println(res.toString());
+		for (DatosComprobante i : res) {
+			System.out.println(i.concepto + "  " + i.monto);
+		}
+
+		assertTrue(true);
+	}
 }
